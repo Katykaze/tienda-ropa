58f20ed40ce0c3524ee69b41f2cfee0858ca6a9e
Katykaze <kathezambrano9@gmail.com>
2023-01-24 14:04:45 +0100

    ....

--- pestanas/login/js/js.js
+++ pestanas/login/js/js.js
@@ -1,1 +1,12 @@
-//programacion para desplegar formulario inicio de sesion o regstro
+//programacion para desplegar formulario registrar de sesion o regstro
+$(window).on("load", inicio);
+
+function inicio() {
+    $("#registrar-sesion").on("click",login_usuario)
+}
+function login_usuario() {
+    console.log("hola");
+    $("#login-registrar").show();
+}
+
+
